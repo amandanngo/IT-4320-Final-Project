1e811b24b458b9c1347b34b7ffa4a19e29c837d8
JoeAxe2027 <jab32@umsystem.edu>
2025-04-28 15:53:56 -0500

    finished admin login function in routes

--- admin/routes.py
+++ admin/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from data_processing import SeatingChart
 
 #Set Up SQLAlchemy
 
@@ -6,9 +7,9 @@ from flask import Blueprint, render_template
 admin_bp = Blueprint('admin', __name__, template_folder='templates', url_prefix='/admin')
 
 # GET route that takes user to login page
-@admin_bp.route('/login')
+@admin_bp.route('/login', methods=['GET'])
 def admin_login():
-    pass
+    return render_template('login.html')
 
 #POST route that attempts to login user
 #Redirects to Admin dashboard upon successful login
